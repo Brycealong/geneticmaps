d5aa2a779d615237e5a64157402416a55b89ff5c
Brycealong <clandestinealong@gmail.com>
2024-08-14 15:31:25 +0800

    aug 14 first commit

--- map2.R
+++ map2.R
@@ -1,34 +1,42 @@
 install.packages("qtl")
 library(qtl)
-https://storage.cloud.google.com/bucket-quickstart_mindful-linker-430503-t8/hq.vcf.gz
-df <- read.csv("hq.vcf.gz.csv", header = FALSE)
-df[2,1] <- ""
-write.table(df, file = "hq.vcf.gz.rpl.csv", sep = ",",
-            quote = FALSE, row.names = FALSE, col.names = FALSE)
-mapthis <- read.cross("csv", "", "hq.vcf.gz.rpl.csv",
+# https://storage.cloud.google.com/bucket-quickstart_mindful-linker-430503-t8/hq.vcf.gz
+# df <- read.csv("hq.vcf.gz.csv", header = FALSE)
+# df[2,1] <- ""
+# write.table(df, file = "hq.vcf.gz.rpl.csv", sep = ",",
+#             quote = FALSE, row.names = FALSE, col.names = FALSE)
+mapthis <- read.cross("csv", "https://storage.googleapis.com/bucket-quickstart_mindful-linker-430503-t8/datasets", "hq.vcf.gz.rpl.csv",
                       estimate.map = FALSE, crosstype = "riself")
 summary(mapthis)
+if (!dir.exists("images")){
+  dir.create("images")
+}
+png("images/plotMissing.png", width = 1200, height = 1200, pointsize = 20)
 plotMissing(mapthis)
+dev.off()
 
+png("images/geno-markers-individuals.png", width = 1200, height = 1200, pointsize = 20)
 par(mfrow=c(1,2), las=1)
 plot(ntyped(mapthis), ylab="No. typed markers", main="No. genotypes by individual") 
 plot(ntyped(mapthis, "mar"), ylab="No. typed individuals", 
      main="No. genotypes by marker")
-mapthis <- subset(mapthis, ind=(ntyped(mapthis)>60000))
+dev.off()
 
+mapthis <- subset(mapthis, ind=(ntyped(mapthis)>60000))
 nt.bymar <- ntyped(mapthis, "mar")
 todrop <- names(nt.bymar[nt.bymar < 250])
 mapthis <- drop.markers(mapthis, todrop)
 
 # Identify duplicate individuals
 cg <- comparegeno(mapthis)
-par(mfrow=c(1,1), las = 1)
-hist(cg[lower.tri(cg)], breaks=seq(0, 1, len=101), xlab="No. matching genotypes") 
+png("images/dup-individuals.png", width = 1200, height = 1200, pointsize = 20)
+hist(cg[lower.tri(cg)], breaks=seq(0, 1, len=101), xlab="No. matching genotypes", main = "") 
 rug(cg[lower.tri(cg)])
+dev.off()
 
 wh <- which(cg > 0.9, arr=TRUE)
 wh <- wh[wh[,1] < wh[,2],]
-wh
+# wh
 
 # g <- pull.geno(mapthis)
 # for(i in 1:nrow(wh)) {
@@ -51,9 +59,12 @@ mapthis <- drop.markers(mapthis, todrop)
 g <- pull.geno(mapthis)
 gfreq <- apply(g, 1, function(a) table(factor(a, levels=1:2)))
 gfreq <- t(t(gfreq) / colSums(gfreq))
+png("images/geno-freq.png", width = 1200, height = 1200, pointsize = 20)
 par(mfrow=c(1,2), las=1)
-for(i in 1:2)
-  {plot(gfreq[i,], ylab="Genotype frequency", main=c("AA", "BB")[i], ylim=c(0,1))}
+for(i in 1:2){
+  plot(gfreq[i,], ylab="Genotype frequency", main=c("AA", "BB")[i], ylim=c(0,1))
+  }
+dev.off()
 
 # Study pairwise marker linkages; look for switched alleles
 mapthis <- est.rf(mapthis)
@@ -61,5 +72,5 @@ checkAlleles(mapthis, threshold=5)
 
 rf <- pull.rf(mapthis)
 lod <- pull.rf(mapthis, what="lod")
-pdf()
+png("images/-individuals.png", width = 1200, height = 1200, pointsize = 20)
 plot(as.numeric(rf), as.numeric(lod), xlab="Recombination fraction", ylab="LOD score")
